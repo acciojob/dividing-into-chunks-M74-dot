ae8cf9a43324e72b3e856167531bc7abfc158c1f
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-10-12 09:22:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let len = arr.length;
+	let start=0;
+	let count=0, currSum=0;
+
+	for(let end=0; end<len; end++){
+		currSum+=arr[end];
+		while(currSum>n && start<=end){
+			currSum-=arr[start]
+			start++;
+		}
+		count+=end-start+1
+	}
+	return count;
 };
 
 const n = prompt("Enter n: ");
